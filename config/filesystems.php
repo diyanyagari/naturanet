2e2a45e275254689b21d2c8c1ad5c164855889d7
Ahmad Izzuddin <ahmadizzuddin150@gmail.com>
2025-08-13 10:56:36 +0700

    Revert "feat(86euda0nb): add CRUD doc upload"
    This reverts commit 9160ce3e670513a696928ca015aa447cdce1770f.

--- config/filesystems.php
+++ config/filesystems.php
@@ -60,13 +60,6 @@ return [
             'report' => false,
         ],
 
-        'cloudinary' => [
-            'driver' => 'cloudinary',
-            'cloud_name' => env('CLOUDINARY_CLOUD_NAME'),
-            'api_key'    => env('CLOUDINARY_API_KEY'),
-            'api_secret' => env('CLOUDINARY_API_SECRET'),
-            'secure'     => env('CLOUDINARY_SECURE', true),
-        ],
     ],
 
     /*
